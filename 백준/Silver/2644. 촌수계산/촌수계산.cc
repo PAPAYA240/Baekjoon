e226e6fa1b18043d2d4dd53fe06276be67234dcc
PAPAYA240 <127913531+PAPAYA240@users.noreply.github.com>
2025-01-27 02:06:13 +0900

    [Silver II] Title: 촌수계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.cc
+++ 백준/Silver/2644. 촌수계산/촌수계산.cc
@@ -4,56 +4,62 @@
 #include <vector>
 using namespace std;
 
-struct Relation {
-    int parents;
-    int child;
+struct Releation
+{
+	int parents = 0;
+	int child = 0;
 };
 
-int N, M, A, B;
-vector<Relation> RelationVec;
-vector<int> kinship; // 각 사람의 촌수를 저장
+int N, M, A, B = 0;
+vector<Releation> ReleationVec;
+vector<int> kinship;
 queue<int> que;
 
-void BFS() {
-    que.push(A);
-    kinship[A] = 0; // A의 촌수는 0으로 초기화
+void BFS()
+{
+	que.push(A);
+	kinship[A] = 0;
 
-    while (!que.empty()) {
-        int current = que.front();
-        que.pop();
+	while (!que.empty())
+	{
+		int value = que.front();
+		que.pop();
 
-        for (const auto& rel : RelationVec) {
-            if (rel.parents == current && kinship[rel.child] == -1) {
-                kinship[rel.child] = kinship[current] + 1; // 촌수 증가
-                que.push(rel.child);
-            } else if (rel.child == current && kinship[rel.parents] == -1) {
-                kinship[rel.parents] = kinship[current] + 1; // 촌수 증가
-                que.push(rel.parents);
-            }
-        }
-    }
+		for (auto& iter : ReleationVec)
+		{
+			if (iter.child == value && -1 == kinship[iter.parents])
+			{
+				kinship[iter.parents] = kinship[iter.child] + 1;
+				que.push(iter.parents);
+			}
+			else if (iter.parents == value && -1 == kinship[iter.child])
+			{
+				kinship[iter.child] = kinship[iter.parents] + 1;
+				que.push(iter.child);
+			}
+		}
+	}
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    cin >> N >> A >> B >> M;
+	// 1. 전체 사람 수 
+	// 2. 촌수 계산 대상
+	// 3. 부모 자식 간 관계 개수
+	cin >> N >> A >> B >> M;
+	kinship.resize(N + 1, -1);
+	kinship.push_back(A);
 
-    // 촌수 배열 초기화
-    kinship.resize(N + 1, -1); // -1로 초기화하여 방문 여부 체크
-
-    // 관계 입력
-    for (int i = 0; i < M; i++) {
-        Relation rel;
-        cin >> rel.parents >> rel.child;
-        RelationVec.push_back(rel);
-    }
-
-    BFS();
-
-    // B의 촌수 출력
-    cout << kinship[B] << endl; 
-
-    return 0;
+	for (int i = 0; i < M; i++)
+	{
+		Releation rel = {};
+		cin >> rel.parents >> rel.child;
+		ReleationVec.push_back(rel);
+	}
+	BFS();
+	cout << kinship[B] << endl;
+	return 0;
 }
